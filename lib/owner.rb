371e8f40bf1b5e4daaf1f3165e7d236a3988b0fd
Tim Knoll <timothy.knoll@gmail.com>
2018-10-17 22:29:19 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,15 @@
 class Owner
-  # code goes here
+  OWNERS = []
+
+  # attr_accessor :pets
+
+  def self.all
+    OWNERS
+  end
+
+  def initialize
+    OWNERS << self
+    # @pets = {:cats => [], :dogs => [], :fish => []}
+  end
+
 end
